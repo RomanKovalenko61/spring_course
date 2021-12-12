4aba1c76df4d02760b91cd8f847ca7cac93ba847
RomanKovalenko61 <astek14@mail.ru>
2021-12-12 23:05:07 +0300

    destroy and init methods

--- src/main/java/com/romankovalenko/spring/Dog.java
+++ src/main/java/com/romankovalenko/spring/Dog.java
@@ -1,19 +1,27 @@
 package com.romankovalenko.spring;
 
 public class Dog implements Pet{
-    private String name;
+//    private String name;
 
     public Dog() {
         System.out.println("Dog bean is created");
     }
 
-    public String getName() {
-        return name;
+    private void init(){
+        System.out.println("Class Dog : init method");
     }
 
-    public void setName(String name) {
-        this.name = name;
+    private void destroy(){
+        System.out.println("Class Dog : destroy method");
     }
+
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 
     @Override
     public void say(){
