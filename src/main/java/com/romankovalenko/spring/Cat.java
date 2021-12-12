b1d77d086c4508ce8aed73d70cbf98bfe81f91d9
RomanKovalenko61 <astek14@mail.ru>
2021-12-12 23:17:18 +0300

    configure with annotation

--- src/main/java/com/romankovalenko/spring/Cat.java
+++ src/main/java/com/romankovalenko/spring/Cat.java
@@ -1,5 +1,8 @@
 package com.romankovalenko.spring;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Cat implements Pet {
     public Cat() {
         System.out.println("Cat bean is created");
