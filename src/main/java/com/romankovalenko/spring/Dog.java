b5ee1d9c8e6d3fdb80682f7cf5028091603c5530
RomanKovalenko61 <astek14@mail.ru>
2021-12-12 22:45:55 +0300

    Bean scope

--- src/main/java/com/romankovalenko/spring/Dog.java
+++ src/main/java/com/romankovalenko/spring/Dog.java
@@ -1,8 +1,18 @@
 package com.romankovalenko.spring;
 
 public class Dog implements Pet{
+    private String name;
+
     public Dog() {
         System.out.println("Dog bean is created");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     @Override
