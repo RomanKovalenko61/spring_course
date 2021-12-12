f24d09609fe53eb47340477d26f49ba00adfc834
RomanKovalenko61 <astek14@mail.ru>
2021-12-12 22:03:32 +0300

    Dependency injections with setter

--- src/main/java/com/romankovalenko/spring/Person.java
+++ src/main/java/com/romankovalenko/spring/Person.java
@@ -3,8 +3,12 @@ package com.romankovalenko.spring;
 public class Person {
     private Pet pet;
 
-    public Person(Pet pet) {
+    public Person() {
         System.out.println("Person bean is created");
+    }
+
+    public void setPet(Pet pet){
+        System.out.println("Class Person: set pet");
         this.pet = pet;
     }
 
