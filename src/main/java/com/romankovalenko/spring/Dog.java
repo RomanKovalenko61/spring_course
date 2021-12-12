841b864116648439653f4b6e7effef8077f19304
RomanKovalenko61 <astek14@mail.ru>
2021-12-12 21:20:51 +0300

    Dependency injections with constructor

--- src/main/java/com/romankovalenko/spring/Dog.java
+++ src/main/java/com/romankovalenko/spring/Dog.java
@@ -1,6 +1,10 @@
 package com.romankovalenko.spring;
 
 public class Dog implements Pet{
+    public Dog() {
+        System.out.println("Dog bean is created");
+    }
+
     @Override
     public void say(){
         System.out.println("Bow-Wow");
