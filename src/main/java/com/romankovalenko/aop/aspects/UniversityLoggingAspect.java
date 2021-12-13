465b1620a414ec188a8d01fdc10e66609971ed88
RomanKovalenko61 <astek14@mail.ru>
2021-12-13 20:22:41 +0300

    Topic 2 - AOP : Advice @AfterThrowing

--- src/main/java/com/romankovalenko/aop/aspects/UniversityLoggingAspect.java
+++ src/main/java/com/romankovalenko/aop/aspects/UniversityLoggingAspect.java
@@ -1,41 +1,46 @@
 package com.romankovalenko.aop.aspects;
 
-import com.romankovalenko.aop.Student;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 @Aspect
 public class UniversityLoggingAspect {
 
-    @Before("execution(* getStudents())")
-    public void beforeGetStudentsLoggingAdvice() {
-        System.out.println("beforeGetStudentsLoggingAdvice: логируем получение " +
-                "списка студентов перед методом getStudents");
-    }
+//    @Before("execution(* getStudents())")
+//    public void beforeGetStudentsLoggingAdvice() {
+//        System.out.println("beforeGetStudentsLoggingAdvice: логируем получение " +
+//                "списка студентов перед методом getStudents");
+//    }
 
-    @AfterReturning(pointcut = "execution(* getStudents())", returning = "students")
-    public void afterReturningStudentsLoggingAdvice(JoinPoint joinPoint, List<Student> students) {
+//    @AfterReturning(pointcut = "execution(* getStudents())", returning = "students")
+//    public void afterReturningStudentsLoggingAdvice(JoinPoint joinPoint, List<Student> students) {
+//
+//        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+//        System.out.println("methodSignature.getMethod() = " + methodSignature.getMethod());
+//
+//        Student firstStudent = students.get(0);
+//        String nameSurname = firstStudent.getNameSurname();
+//        nameSurname = "Mr. " + nameSurname;
+//        firstStudent.setNameSurname(nameSurname);
+//
+//        double avgGrade = firstStudent.getAvgGrade();
+//        avgGrade = avgGrade + 1;
+//        firstStudent.setAvgGrade(avgGrade);
+//
+//        System.out.println("afterReturningStudentsLoggingAdvice: логируем получение " +
+//                "списка студентов после работы метода getStudents");
+//    }
 
+    @AfterThrowing(pointcut = "execution(* getStudents())"
+            , throwing = "exception")
+    public void afterThrowingStudentsLoggingAdvice(JoinPoint joinPoint, Throwable exception) {
         MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
         System.out.println("methodSignature.getMethod() = " + methodSignature.getMethod());
-
-        Student firstStudent = students.get(0);
-        String nameSurname = firstStudent.getNameSurname();
-        nameSurname = "Mr. " + nameSurname;
-        firstStudent.setNameSurname(nameSurname);
-
-        double avgGrade = firstStudent.getAvgGrade();
-        avgGrade = avgGrade + 1;
-        firstStudent.setAvgGrade(avgGrade);
-
-        System.out.println("afterReturningStudentsLoggingAdvice: логируем получение " +
-                "списка студентов после работы метода getStudents");
+        // Обработать нельзя, исключение пробросится дальше. (Можно залогировать например)
+        System.out.println("afterThrowingStudentsLoggingAdvice: логируем выброс исключения " + exception);
     }
 }
