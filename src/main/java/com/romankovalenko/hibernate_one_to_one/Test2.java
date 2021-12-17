88dac7ff889efe27ccd394534c34184b45e4f0ab
RomanKovalenko61 <astek14@mail.ru>
2021-12-17 19:54:20 +0300

    refactoring - rename package

--- src/main/java/com/romankovalenko/hibernate_one_to_one/Test2.java
+++ src/main/java/com/romankovalenko/hibernate_one_to_one/Test2.java
@@ -1,7 +1,7 @@
-package com.romankovalenko.hibernate_test2;
+package com.romankovalenko.hibernate_one_to_one;
 
-import com.romankovalenko.hibernate_test2.entity.Detail;
-import com.romankovalenko.hibernate_test2.entity.Employee;
+import com.romankovalenko.hibernate_one_to_one.entity.Detail;
+import com.romankovalenko.hibernate_one_to_one.entity.Employee;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
--- src/main/java/com/romankovalenko/hibernate_one_to_one/entity/Detail.java
+++ src/main/java/com/romankovalenko/hibernate_one_to_one/entity/Detail.java
@@ -1,4 +1,4 @@
-package com.romankovalenko.hibernate_test2.entity;
+package com.romankovalenko.hibernate_one_to_one.entity;
 
 import javax.persistence.*;
 
