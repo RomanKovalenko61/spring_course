8070e6982d95f9d6387363da92474371c21f1b73
kb3-3 <astek14@mail.ru>
2023-07-12 11:47:39 +0300

    add new tables employees and details

--- resources/table.sql
+++ resources/table.sql
@@ -1,9 +1,32 @@
 
 CREATE TABLE employees (
     id SERIAL,
-    name varchar(15),
-    surname varchar(25),
-    department varchar(20),
-    salary int,
+    name VARCHAR(15),
+    surname VARCHAR(25),
+    department VARCHAR(20),
+    salary INT,
     PRIMARY KEY (id)
+);
+
+-- for hibernate relations lessons
+
+DROP TABLE employees;
+
+CREATE TABLE details (
+    id SERIAL,
+    city VARCHAR(15),
+    phone_number VARCHAR(25),
+    email VARCHAR(30),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employees (
+    id SERIAL,
+    name VARCHAR(15),
+    surname VARCHAR(25),
+    department VARCHAR(20),
+    salary INT,
+    details_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (details_id) REFERENCES details(id)
 );
