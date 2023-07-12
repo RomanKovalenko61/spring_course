6605a061e7e785b789bbd085763643e4a125979a
kb3-3 <astek14@mail.ru>
2023-07-12 14:44:22 +0300

    add new tables employees and departments

--- resources/table.sql
+++ resources/table.sql
@@ -30,3 +30,26 @@ CREATE TABLE employees (
     PRIMARY KEY (id),
     FOREIGN KEY (details_id) REFERENCES details(id)
 );
+
+-- one-to-many
+
+DROP TABLE employees;
+DROP TABLE details;
+
+CREATE TABLE departments (
+    id SERIAL,
+    name VARCHAR(15),
+    max_salary INT,
+    min_salary INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employees (
+    id SERIAL,
+    name VARCHAR(15),
+    surname VARCHAR(25),
+    salary INT,
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES departments(id)
+);
